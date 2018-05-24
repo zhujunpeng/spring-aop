a2ca4293c75161287cf3d56dad9036e5d7d655bf
朱君鹏 <zhujunpeng@ijovo.com>
2018-05-24 20:42:19 +0800

    frist commit

---
+++ src/main/java/org/springframework/aop/config/AopNamespaceUtils.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aop.config;
+
+import org.w3c.dom.Element;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.parsing.BeanComponentDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.ParserContext;
+
+/**
+ * Utility class for handling registration of auto-proxy creators used internally
+ * by the '{@code aop}' namespace tags.
+ *
+ * <p>Only a single auto-proxy creator can be registered and multiple tags may wish
+ * to register different concrete implementations. As such this class delegates to
+ * {@link AopConfigUtils} which wraps a simple escalation protocol. Therefore classes
+ * may request a particular auto-proxy creator and know that class, <i>or a subclass
+ * thereof</i>, will eventually be resident in the application context.
+ *
+ * @author Rob Harrop
+ * @author Juergen Hoeller
+ * @author Mark Fisher
+ * @since 2.0
+ * @see AopConfigUtils
+ */
+public abstract class AopNamespaceUtils {
+
+	/**
+	 * The {@code proxy-target-class} attribute as found on AOP-related XML tags.
+	 */
+	public static final String PROXY_TARGET_CLASS_ATTRIBUTE = "proxy-target-class";
+
+	/**
+	 * The {@code expose-proxy} attribute as found on AOP-related XML tags.
+	 */
+	private static final String EXPOSE_PROXY_ATTRIBUTE = "expose-proxy";
+
+
+	public static void registerAutoProxyCreatorIfNecessary(
+			ParserContext parserContext, Element sourceElement) {
+
+		BeanDefinition beanDefinition = AopConfigUtils.registerAutoProxyCreatorIfNecessary(
+				parserContext.getRegistry(), parserContext.extractSource(sourceElement));
+		useClassProxyingIfNecessary(parserContext.getRegistry(), sourceElement);
+		registerComponentIfNecessary(beanDefinition, parserContext);
+	}
+
+	public static void registerAspectJAutoProxyCreatorIfNecessary(
+			ParserContext parserContext, Element sourceElement) {
+
+		BeanDefinition beanDefinition = AopConfigUtils.registerAspectJAutoProxyCreatorIfNecessary(
+				parserContext.getRegistry(), parserContext.extractSource(sourceElement));
+		useClassProxyingIfNecessary(parserContext.getRegistry(), sourceElement);
+		registerComponentIfNecessary(beanDefinition, parserContext);
+	}
+
+	public static void registerAspectJAnnotationAutoProxyCreatorIfNecessary(
+			ParserContext parserContext, Element sourceElement) {
+		// 1、注册或升级AutoProxyCreator定义beanName 为org.Springframework.aop.config.internalAutoProxyCreator的BeanDefinition
+		BeanDefinition beanDefinition = AopConfigUtils.registerAspectJAnnotationAutoProxyCreatorIfNecessary(
+				parserContext.getRegistry(), parserContext.extractSource(sourceElement));
+		// 2、对于proxy-target-class以及expose-proxy属性处理
+		// proxy-target-class使用哪种方式进行动态代理，expose-proxy是否暴露
+		useClassProxyingIfNecessary(parserContext.getRegistry(), sourceElement);
+		// 3、注册组件并通知，便于监听器做进一步处理
+		// 其中beanDefinition的className为AnnotationAwareAspectJAutoProxyCreator
+		registerComponentIfNecessary(beanDefinition, parserContext);
+	}
+
+	/**
+	 * @deprecated since Spring 2.5, in favor of
+	 * {@link #registerAutoProxyCreatorIfNecessary(ParserContext, Element)} and
+	 * {@link AopConfigUtils#registerAutoProxyCreatorIfNecessary(BeanDefinitionRegistry, Object)}
+	 */
+	@Deprecated
+	public static void registerAutoProxyCreatorIfNecessary(ParserContext parserContext, Object source) {
+		BeanDefinition beanDefinition = AopConfigUtils.registerAutoProxyCreatorIfNecessary(
+				parserContext.getRegistry(), source);
+		registerComponentIfNecessary(beanDefinition, parserContext);
+	}
+
+	/**
+	 * @deprecated since Spring 2.5, in favor of
+	 * {@link AopConfigUtils#forceAutoProxyCreatorToUseClassProxying(BeanDefinitionRegistry)}
+	 */
+	@Deprecated
+	public static void forceAutoProxyCreatorToUseClassProxying(BeanDefinitionRegistry registry) {
+		AopConfigUtils.forceAutoProxyCreatorToUseClassProxying(registry);
+	}
+
+	/**
+	 * 对于proxy-target-class以及expose-proxy属性处理
+	 * 
+	 * 
+	 * proxy-target-class属性值决定是基于接口的还是基于类的代理被创建，如果是true那么将使用类代理（cglib），
+	 * 即使是未声明，但运行类没有继承接口，spring也会自动使用CGLIB代理，
+	 * 但是在spring高版本中会根据运行类进行选择JDK或者cglib代理
+	 * 
+	 * expose-proxy="true" 当前代理是否为可暴露状态,值是"ture",则为可访问。
+	 * @param registry
+	 * @param sourceElement
+	 */
+	private static void useClassProxyingIfNecessary(BeanDefinitionRegistry registry, Element sourceElement) {
+		if (sourceElement != null) {
+			// 对proxy-target-class属性的处理
+			boolean proxyTargetClass = Boolean.valueOf(sourceElement.getAttribute(PROXY_TARGET_CLASS_ATTRIBUTE));
+			if (proxyTargetClass) {
+				AopConfigUtils.forceAutoProxyCreatorToUseClassProxying(registry);
+			}
+			// 对expose-proxy属性的处理
+			boolean exposeProxy = Boolean.valueOf(sourceElement.getAttribute(EXPOSE_PROXY_ATTRIBUTE));
+			if (exposeProxy) {
+				AopConfigUtils.forceAutoProxyCreatorToExposeProxy(registry);
+			}
+		}
+	}
+
+	private static void registerComponentIfNecessary(BeanDefinition beanDefinition, ParserContext parserContext) {
+		if (beanDefinition != null) {
+			BeanComponentDefinition componentDefinition =
+					new BeanComponentDefinition(beanDefinition, AopConfigUtils.AUTO_PROXY_CREATOR_BEAN_NAME);
+			parserContext.registerComponent(componentDefinition);
+		}
+	}
+
+}
